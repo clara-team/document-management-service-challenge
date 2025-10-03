bd22d0e7486205ba8312b707237170f140738859
Ana Castro <ana.castro@mail.com>
2025-10-03 02:22:26 -0600

    chore(infra): update DB init script and local env variables

--- docker/init-scripts/schema-init.sql
+++ docker/init-scripts/schema-init.sql
@@ -1,5 +1,16 @@
 --The script to initialize the schema was sourced from the Spring Batch Core dependency: org.springframework.batch.core.
-
-CREATE SCHEMA document_schema;
+CREATE SCHEMA IF NOT EXISTS document_schema;
 SET SCHEMA 'document_schema';
-
+CREATE TABLE IF NOT EXISTS document_schema.documents (
+                                                         id BIGSERIAL PRIMARY KEY,
+                                                         user_id BIGINT NOT NULL,
+                                                         document_name VARCHAR(255) NOT NULL,
+                                                         tags TEXT, -- o JSONB si prefieres
+                                                         minio_path VARCHAR(512),
+                                                         file_size BIGINT,
+                                                         file_type VARCHAR(50),
+                                                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                                         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                                         checksum VARCHAR(255),
+                                                         status VARCHAR(50)
+);
